975a41fb0e347c4743ff75ed3e594c47232d4dce
ahmed_mamdouh <ahmedmamdouh.am32@gmail.com>
2024-10-27 22:48:58 +0300

    First version

---
+++ AudioClip.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Media;
+using System.Timers;
+using System.Diagnostics;
+using NAudio.Wave;
+using System.Diagnostics;
+namespace MediaPlayerFramework
+{
+    internal class AudioClip
+    {
+        public string name { set; get; }
+        public string path {  set; get; }
+        public string duration {  set; get; }
+        public bool finished { get; private set; }
+
+        private  int min;
+        private  int sec;
+
+        private int duration_seconds;
+        private int SecondsCounter = 0;
+
+        private int ConsoleX;
+        private int ConsoleY;
+        Timer timer;
+
+        private AudioFileReader audioFile;
+        private WaveOutEvent outputDevice;
+
+        Stopwatch stopwatch = new Stopwatch();
+        private double previous_time = 0;
+
+        public AudioClip(string name,string path,string duration ) 
+        { 
+            this.name = name;
+            this.path = path;
+            outputDevice = new WaveOutEvent();
+            audioFile = new AudioFileReader(path);
+            this.duration = duration;
+            string[] parts = duration.Split(':');
+            int min = int.Parse(parts[0]);
+            int sec = int.Parse(parts[1]);
+            duration_seconds = min * 60 + sec;
+        }
+
+        public void Play()
+        {
+            timer = new Timer(1000); // 1000 ms = 1 second
+            timer.Elapsed += OnTimedEvent;  // Event handler for each tick
+            timer.AutoReset = true;         // Resets after each interval
+            timer.Enabled = true;           // Start the timer
+            stopwatch.Start();
+            previous_time = stopwatch.Elapsed.TotalSeconds;
+            finished = false;
+            outputDevice.Init(audioFile);
+            Console.WriteLine($"Now playing {name}");
+            ConsoleX = Console.CursorLeft;  //to get initial position of the console
+            ConsoleY = Console.CursorTop;
+            Console.WriteLine($"{name}: {min:D2}:{sec:D2}       {(float)((float)SecondsCounter / duration_seconds) * 100:f2}%");
+            outputDevice.Play();
+        }
+
+        public void Stop()
+        {
+            outputDevice.Stop();
+            Console.WriteLine($"{name} has stopped");
+            timer.Stop();
+            timer.Dispose();
+            finished = true;
+            sec = 0;
+            min = 0;
+            SecondsCounter = 0;
+            stopwatch.Reset();
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            double elapsed_time = stopwatch.Elapsed.TotalSeconds;
+            double passed_time = (stopwatch.Elapsed.TotalSeconds - previous_time);
+            if (stopwatch.Elapsed.TotalSeconds - previous_time >= 1)
+            {
+                previous_time = stopwatch.Elapsed.TotalSeconds;
+                if(passed_time > 1)
+                {
+                    sec += (int)passed_time;
+                    SecondsCounter += (int)passed_time;
+                }
+                else
+                {
+                    sec++;
+                    SecondsCounter++;
+                }               
+
+                if (sec >= 60) // Increment minutes if 60 seconds pass
+                {
+                    sec = 0;
+                    min++;
+                }
+                Console.SetCursorPosition(ConsoleX, ConsoleY);
+                Console.WriteLine($"{name}: {min:D2}:{sec:D2}       {(float)((float)SecondsCounter / duration_seconds) * 100:f2}%");
+                if (duration == $"{min:D2}:{sec:D2}")
+                {
+                    timer.Stop();
+                    timer.Dispose();
+                    finished = true;
+                    Console.WriteLine($"{name} finished");
+                }
+            }
+        }
+
+        public void PlayStream()
+        {
+            Play();
+            while (finished == false)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyPressed = Console.ReadKey(true); // Read the key without displaying it
+
+                    if (keyPressed.Key == ConsoleKey.Spacebar) //puase button
+                    {
+                        if (outputDevice.PlaybackState == PlaybackState.Paused)
+                        {
+                            outputDevice.Play();
+                            timer.Enabled = true;           // Start the timer
+                            stopwatch.Start();
+                        }
+                        else
+                        {
+                            outputDevice.Pause();
+                            timer.Stop();
+                            stopwatch.Stop();
+                        }
+                    }
+                }
+            }
+            //the audio is finished
+            min = 0;
+            sec = 0;
+            SecondsCounter = 0;
+            outputDevice.Dispose();
+            timer.Dispose();
+            stopwatch.Reset();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using NAudio.Wave;
+namespace MediaPlayerFramework
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+           
+
+
+            AudioClip sound1 = new AudioClip(name: "Sample 1", path: "C:\\Users\\adminstrator\\Downloads\\sample1.mp3", duration: "00:07");
+            AudioClip sound2 = new AudioClip(name: "Sample 2", path: "C:\\Users\\adminstrator\\Downloads\\sample2.mp3", duration: "00:10");
+            AudioClip sound3 = new AudioClip(name: "Sample 3", path: "C:\\Users\\adminstrator\\Downloads\\sample3.mp3", duration: "00:13");
+
+         
+
+            sound2.PlayStream();
+
+            sound1.PlayStream();
+
+            sound3.PlayStream();
+
+            Console.ReadLine();
+
+
+          
+        }
+
+        public static void PlayMusic(string FilePath)
+        {
+            SoundPlayer MusicPlayer = new SoundPlayer();
+            MusicPlayer.SoundLocation = FilePath;
+            MusicPlayer.Play();
+        
+            
+
+        }
+        public static void StopMusic(string FilePath)
+        {
+            SoundPlayer MusicPlayer = new SoundPlayer();
+            MusicPlayer.SoundLocation = FilePath;
+            MusicPlayer.Stop();
+            
+
+        }
+
+        public static void print_on_cursor(int x, int y, string word)
+        {
+            //Console.Clear();
+            Console.SetCursorPosition(x,y);
+            Console.WriteLine(word);
+        } 
+
+        public static string seconds_to_time(int seconds)
+        {
+            string min_str;
+            string sec_str;
+            int min = seconds / 60;
+            int sec = seconds % 60;
+
+            //Console.WriteLine($"minutes : {min}, Seconds : {sec}");
+            if(min < 10)
+            {
+                min_str = $"0{min}";
+            }
+            else
+            {
+                min_str = min.ToString();
+            }
+
+            if (sec < 10)
+            {
+                sec_str = $"0{sec}";
+            }
+            else
+            {
+                sec_str= sec.ToString();
+            }
+
+            return $"{min_str}:{sec_str}";
+
+        }
+
+    }
+}
+
+
+
